fe4561f0952e6d387cff59eb9b0fb3ee4e65d282
Bishop Walker <bishop@northernneckgarbage.com>
2025-10-05 22:19:45 -0400

    Revert "fix: Resolve GitLab CI/CD pipeline issues"
    This reverts commit b548efc7eeb09b6fad4e5e245d68d9eaf21047e0.

--- backend/mocks/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Mock modules for CI/CD testing."""
-
-import sys
-import os
-
-# Add this directory to Python path for importing mocks
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
